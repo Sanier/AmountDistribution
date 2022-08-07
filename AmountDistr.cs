c21c86b7f4880db06f83f137a088359d02e181f0
Alexandr <sanier20@mail.ru>
2022-08-07 19:50:42 +0300

    Добавьте файлы проекта.

---
+++ AmountDistr.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmountDistribution
+{
+    public class AmountDistr
+    {
+        public void AmountDistrub(string name, float summ, string[] sums)
+        {
+            //Переменная для подсчета коэффициента
+            float koef = summ / sums.Select(float.Parse).Sum();
+
+            float num = 0;
+          
+            if (name == "ПРОП" || name == "проп")
+            {
+                float summa = 0;
+                for (int i = 0; i < sums.Length; i++)
+                {
+                    num = MathF.Round(float.Parse(sums[i]) * koef, 2);
+                    summa += num;
+                    sums[i] = Convert.ToString(num);                    
+                }
+
+                if (summa < summ)
+                {
+                    num += MathF.Round(summ - summa, 2);
+                    sums[sums.Length - 1] = Convert.ToString(num);
+                }
+
+                //Вывод массива
+                foreach (string s in sums)
+                {
+                    Console.Write("{0};", s);
+                }
+            }
+
+            if (name == "ПЕРВ" || name == "певр")
+            {
+                num = summ;
+                for (int i = 0; i < sums.Length; i++)
+                {
+                    if (num >= float.Parse(sums[i]))
+                    {
+                        num -= float.Parse(sums[i]);
+                    }
+                    else if (num > 0)
+                    {
+                        sums[i] = Convert.ToString(num);
+                        num = 0;
+                    }
+                    else
+                    {
+                        sums[i] = "0";
+                    }
+                }
+
+                //Вывод массива
+                foreach (string s in sums)
+                {
+                    Console.Write("{0};", s);
+                }
+            }
+
+            if (name == "ПОСЛ" || name == "посл")
+            {
+                num = summ;
+                //перевод в массив Array через LINQ
+                var result = sums.Select(c => new string(c.ToArray())).ToArray();
+                //Разворот массива для работы с последними элементами
+                Array.Reverse(result);
+
+                for (int i = 0; i < sums.Length; i++)
+                {
+                    if (num >= float.Parse(result[i]))
+                    {
+                        num -= float.Parse(result[i]);
+                    }
+                    else if (num > 0)
+                    {
+                        result[i] = Convert.ToString(num);
+                        num = 0;
+                    }
+                    else
+                    {
+                        result[i] = "0";
+                    }
+                }
+                //Разворот обратно
+                Array.Reverse(result);
+
+                //Вывод массива
+                foreach (string s in result)
+                {
+                    Console.Write("{0};", s);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿AmountDistribution.AmountDistr amountDistr = new AmountDistribution.AmountDistr();
+
+Console.WriteLine("Введите тип распределения (ПОСЛ, ПЕРВ, ПРОП)");
+string name = Console.ReadLine();
+
+Console.WriteLine("Введите распределяемую сумму");
+float summ = MathF.Round(float.Parse(Console.ReadLine(), (System.Globalization.NumberStyles)2));
+
+Console.Write("Введите количество сумм, которые хотите ввести: ");
+int n = Convert.ToInt32(Console.ReadLine()); // Считываем строку, переводим в число
+string[] strs = new string[n]; //Объявляем массив строк длиной n (которую ввёл пользователь)
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Введите строку №{0}:\r\n    ", i + 1);
+    strs[i] = Console.ReadLine(); //Заполняем его
+}
+
+amountDistr.AmountDistrub(name, summ, strs);
